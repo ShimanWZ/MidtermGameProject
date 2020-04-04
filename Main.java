041c40d9b6aa282db8b025d508d72bce293daec5
zeinmzadeh <58462222+zeinmzadeh@users.noreply.github.com>
2020-04-04 21:15:16 +0430

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,17 +1,34 @@
-package GameCore;
-
-public class Main {
-
-	public static void main(String[] args) {
-		Game game = new Game();
-		HeadQuarter soldier = new HeadQuarter();
-		soldier.place(5, 5, game.getPlayer1());
-		for (int i = 0 ; i < Game.boardSize ; i++) {
-			for( int j = 0 ; j < Game.boardSize ; j++) {
-				System.out.println(game.getPlayer1().getGameBoard().getBoardArray()[i][j]);
-			}
-		}
+package UI;
 	
-	}
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 
+
+public class Main extends Application {
+	
+	static Stage window;
+	Scene mainMenu;
+	static Scene settingMenu;
+	
+	@Override
+	public void start(Stage primaryStage) {
+		window = primaryStage;
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("MainMenue.fxml"));
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			window.setScene(scene);
+			window.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
